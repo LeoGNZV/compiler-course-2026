a5d911719af9f94d66d03d580b6bfb88ecabb9b6
root <root@DESKTOP-628S40D.localdomain>
2026-04-03 21:51:35 +0300

    clang-format change

--- llvm/compiler-course/llvm-ir/gonozov_l_lab_2/ExamplePass.cpp
+++ llvm/compiler-course/llvm-ir/gonozov_l_lab_2/ExamplePass.cpp
@@ -16,35 +16,24 @@ struct ExamplePass : llvm::PassInfoMixin<ExamplePass> {
         if (auto *callInst = llvm::dyn_cast<llvm::CallInst>(&instr)) {
           if (llvm::Function *calledFunc = callInst->getCalledFunction()) {
             llvm::Intrinsic::ID id = calledFunc->getIntrinsicID();
-            if (id == llvm::Intrinsic::powi)
-            {
-              auto *lhs = callInst->getOperand(0); //getArgOperand?
+            if (id == llvm::Intrinsic::powi) {
+              auto *lhs = callInst->getOperand(0);
               auto *rhs = callInst->getOperand(1);
-
-              int64_t power = llvm::dyn_cast<llvm::ConstantInt>(rhs)->getSExtValue();
-              if (power >= 0 && power <= 4)
-              {
+              int64_t power =
+                   llvm::dyn_cast<llvm::ConstantInt>(rhs)->getSExtValue();
+              if (power >= 0 && power <= 4) {
                 llvm::IRBuilder<> builder(callInst);
                 llvm::Value *newValue = nullptr;
-                if (power == 0)
-                {
+                if (power == 0) {
                   newValue = llvm::ConstantFP::get(lhs->getType(), 1.0);
-                }
-                else if (power == 1)
-                {
+                } else if (power == 1) {
                   newValue = lhs;
-                }
-                else if (power == 2)
-                {
+                } else if (power == 2) {
                   newValue = builder.CreateFMul(lhs, lhs);
-                }
-                else if (power == 3)
-                {
+                } else if (power == 3) {
                   auto *intermediate = builder.CreateFMul(lhs, lhs);
                   newValue = builder.CreateFMul(intermediate, lhs);
-                }
-                else if (power == 4)
-                {
+                } else if (power == 4) {
                   auto *intermediate = builder.CreateFMul(lhs, lhs);
                   newValue = builder.CreateFMul(intermediate, intermediate);
                 }
